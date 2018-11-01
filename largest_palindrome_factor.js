c4bcca1b57eaa96ee6f0d22b0911000476ede552
Ashe Austaire <asheaustaire@gmail.com>
2018-11-01 14:11:10 -0400

    initialize_largest_palindrome_factor

---
+++ largest_palindrome_factor.js
@@ -0,0 +1,15 @@
+
+// 99, 595, 3333, 13
+
+function largestPalindromeFactor(num) {
+
+  function checkPalindrome(num) {
+
+    let string = String(num);
+
+    for(let i = num.length; i > 0; i-- ) {
+      console.log(string[i]);
+    }
+  }
+
+}

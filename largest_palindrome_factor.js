f39ab897de2c6492ecdee200932d85d2fbdeef23
Ashe Austaire <asheaustaire@gmail.com>
2018-11-01 16:08:20 -0400

    finished_largest_palindrome_factor

--- largest_palindrome_factor.js
+++ largest_palindrome_factor.js
@@ -1,15 +1,48 @@
+function largestPalindromeFactor() {
 
-// 99, 595, 3333, 13
+  let currentGreatest = 0;
 
-function largestPalindromeFactor(num) {
-
-  function checkPalindrome(num) {
-
-    let string = String(num);
-
-    for(let i = num.length; i > 0; i-- ) {
-      console.log(string[i]);
+  for(let i = 100; i <= 999; i++) {
+    for( let i2 = 100; i2 <= 999; i2++) {
+      if ( checkPalindrome((i * i2)) ) {
+        if (currentGreatest < i * i2) {
+          currentGreatest = i * i2;
+        }
+      }
+      
     }
   }
 
+  return currentGreatest
+
+  function checkPalindrome(num) {
+
+    let numString = String(num);
+    let checkArr = [];
+    let newString = ""
+
+    for( let i = 0; i < numString.length; i++) {
+
+      checkArr.unshift(numString[i]);
+
+    }
+
+    newString = checkArr.join("");
+    
+    if (newString === numString) { return true }
+    else {return false}
+    
+  }
+  
+  function returnGreater(num, num2) {
+
+    let greatestNum = num > num2 ? num : num2;
+    return greatestNum;
+    
+  }
+  
 }
+
+console.log(largestPalindromeFactor());
+
+
